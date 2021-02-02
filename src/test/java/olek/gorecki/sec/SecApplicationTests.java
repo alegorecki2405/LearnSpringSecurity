16385d2c8270217375f5dae58b3fa5918d1228ad
Aleksander Gorecki <alegorecki@gmail.com>
2021-02-03 00:53:39 +0100

    Initial commit

---
+++ src/test/java/olek/gorecki/sec/SecApplicationTests.java
@@ -0,0 +1,13 @@
+package olek.gorecki.sec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
