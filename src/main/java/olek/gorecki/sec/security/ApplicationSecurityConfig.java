87ef63dc20ae7594aacf37c28786675d62fdab26
Aleksander Gorecki <alegorecki@gmail.com>
2021-02-13 00:07:41 +0100

    final jwt authorization i have to refactor this a bit

--- src/main/java/olek/gorecki/sec/security/ApplicationSecurityConfig.java
+++ src/main/java/olek/gorecki/sec/security/ApplicationSecurityConfig.java
@@ -1,6 +1,7 @@
 package olek.gorecki.sec.security;
 
 import olek.gorecki.sec.auth.ApplicationUserService;
+import olek.gorecki.sec.jwt.JwtTokenVerifier;
 import olek.gorecki.sec.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -40,6 +41,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))
+                .addFilterAfter(new JwtTokenVerifier(),JwtUsernameAndPasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
